6e80cb7ee58553e704f09f8122f3c2b7f23e6807
MarlonAngonese <marlonmaster2020@gmail.com>
2022-05-06 23:46:17 -0300

    Tela do Volume Útil e Tela de Seleção do Tipo de Edificação

--- main.py
+++ main.py
@@ -1,27 +1,113 @@
 from kivy.lang import Builder
-from kivy.metrics import dp
-from kivy.properties import StringProperty
+from kivy.core.window import Window
 
 from kivymd.app import MDApp
-from kivymd.uix.menu import MDDropdownMenu
-
 from kivy.uix.screenmanager import ScreenManager
 from kivymd.uix.screen import MDScreen
+from kivymd.uix.menu import MDDropdownMenu
 
-class Manager(ScreenManager): 
+Window.size = (720 , 640)
+class Manager(ScreenManager):
     pass
 
-class VolumetriaScreen(MDScreen):
+class CalculoDeVolumeScreen(MDScreen):
     pass
 
-class Test(MDApp):
+class TipoDeEdificacaoScreen(MDScreen):
+    pass
+
+class Fossilita(MDApp):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         
-        self.screen = Builder.load_file('./main.kv')
+        self.screenmanager = Builder.load_file('./main.kv')
 
     def build(self):
-        return self.screen
+        return self.screenmanager
+    
+    def change_screen(self, button, target):
+        self.screenmanager.current = "tipo_de_edificacao"
+        self.screenmanager.transition.direction = "left"
 
-Test().run()
+        self.screenmanager.get_screen('tipo_de_edificacao').ids.tipo_de_edificacao_scrollview.size = (Window.width, Window.height)
+
+    def calculate_volume_util(self):
+
+        Edificacao = self.screenmanager.get_screen('calculo_de_volume').ids.dropdown_tipo_de_edificacao.text # Tipo de Edificação
+        Np = int(self.screenmanager.get_screen('calculo_de_volume').ids.textbox_numero_de_pessoas.text) # Número de Pessoas
+        Temperatura_media = int(self.screenmanager.get_screen('calculo_de_volume').ids.textbox_temperatura_media.text)  # Temperatura Média
+
+        C = False #Contribuição de Esgoto por Unidade
+        T = False #Tempo de detenção
+        Lf = False #Contribuição de Lodo Fresco
+        Cd = False #Contribuição Diária
+        K = False #Taxa de acumulação de Lodo Fresco
+
+        if (Edificacao == "Residência"):
+            C = 200
+            Lf = 1
+        elif (Edificacao == "Alojamento Provisório"):
+            C = 80
+            Lf = 1
+        elif (Edificacao == "Fábrica em Geral"):
+            C = 70
+            Lf = 0.3
+        elif (Edificacao == "Escritório"):
+            C = 50
+            Lf = 0.2
+        elif (Edificacao == "Edifícios Públicos ou Comerciais"):
+            C = 50
+            Lf = 0.2
+        elif (Edificacao == "Escolas ou Locais de Longa Permanência"):
+            C = 50
+            Lf = 0.2
+        elif (Edificacao == "Bares"):
+            C = 6
+            Lf = 0.1
+        elif (Edificacao == "Restaurantes e Similares"):
+            C = 25
+            Lf = 0.1
+        elif (Edificacao == "Cinemas, Teatros e Locais de Curta Permanência"):
+            C = 2
+            Lf = 0.02
+        elif (Edificacao == "Sanitários Públicos"):
+            C = 480
+            Lf = 4
+
+        Cd = Np * C  #Cálculo da Contribuição Diária
+
+        if (Cd <= 1500):
+            T = 24
+        elif (Cd >= 1501 and Cd <= 3000):
+            T = 22
+        elif (Cd >= 3001 and Cd <= 4500):
+            T = 20
+        elif (Cd >= 4501 and Cd <= 6000):
+            T = 18
+        elif (Cd >= 6001 and Cd <= 7500):
+            T = 16
+        elif (Cd >= 7501 and Cd <= 9000):
+            T = 14
+        elif (Cd >= 9001):
+            T = 12
+
+        if (Temperatura_media < 10):
+            K = 254
+        elif (Temperatura_media >= 11 and Temperatura_media <= 20):
+            K = 225
+
+        elif (Temperatura_media >= 21):
+            K = 217
+
+        Volume_util = 1000 + Np * (C * T + K + Lf)
+
+        self.screenmanager.get_screen('calculo_de_volume').ids.label_mostrar_resultado.text = "Volume Útil: " + str(Volume_util) + " litros"
+
+    def choose_option_tipo_de_edificacao(self, option):
+        self.screenmanager.current = "calculo_de_volume"
+        self.screenmanager.transition.direction = "right"
+
+        self.screenmanager.get_screen('calculo_de_volume').ids.dropdown_tipo_de_edificacao.text = option.text
+
+Fossilita().run()
