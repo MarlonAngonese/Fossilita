3bf896d36fc53784cf29d2c9509bd17f42e78a29
MarlonAngonese <marlonmaster2020@gmail.com>
2022-06-24 02:06:28 -0300

    Add last screen and set validation

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ class Manager(ScreenManager):
             if (APP.screenmanager.current == 'home'):
                 return True
 
-            if (APP.screenmanager.current == 'calculo_de_volume' or APP.screenmanager.current == 'filtro_anaerobio' or APP.screenmanager.current == 'informacoes_importantes'):
+            if (APP.screenmanager.current == 'calculo_de_volume' or APP.screenmanager.current == 'filtro_anaerobio' or APP.screenmanager.current == "sumidouro" or APP.screenmanager.current == 'informacoes_importantes'):
                 APP.screenmanager.current = 'home'
                 APP.screenmanager.transition.direction = "right"
                 return True
@@ -38,6 +38,10 @@ class Manager(ScreenManager):
 
             if (APP.screenmanager.current == 'resultado_filtro_anaerobio'):
                 APP.return_right_to('filtro_anaerobio')
+                return True
+
+            if (APP.screenmanager.current == 'resultado_sumidouro'):
+                APP.return_right_to('sumidouro')
                 return True
 
             if (APP.screenmanager.current == 'intervalo_de_limpeza' or APP.screenmanager.current == 'funcionamento_tanque_septico' or APP.screenmanager.current == 'dimensionamento_tanque_septico' or APP.screenmanager.current == 'dimensionamento_vala_infiltracao' or APP.screenmanager.current == 'tipos_sumidouros' or APP.screenmanager.current == 'manual_boas_praticas'):
@@ -62,9 +66,14 @@ class ResultadoFossaScreen(MDScreen):
 class ResultadoFiltroAnaerobioScreen(MDScreen):
     pass
 
+class ResultadoSumidouroScreen(MDScreen):
+    pass
+
 class FiltroAnaerobioScreen(MDScreen):
     pass
 
+class SumidouroScreen(MDScreen):
+    pass
 # PDFS
 class IntervaloDeLimpezaScreen(MDScreen):
     pass
@@ -96,6 +105,34 @@ class Fossilita(MDApp):
         self.theme_cls.primary_palette = "Indigo"
         self.theme_cls.primary_hue = '900'
         return self.screenmanager
+
+    def clear_all(self):
+        # Fossa
+        self.screenmanager.get_screen('calculo_de_volume').ids.dropdown_tipo_de_edificacao.text = "Selecione uma opção"
+        self.screenmanager.get_screen('calculo_de_volume').ids.textbox_numero_de_pessoas.text = ""
+        self.screenmanager.get_screen('calculo_de_volume').ids.textbox_temperatura_media.text = ""
+        self.screenmanager.get_screen('calculo_de_volume').ids.textbox_intervalo_limpeza.text = ""
+
+        self.screenmanager.get_screen('calculo_de_volume').ids.dropdown_tipo_de_edificacao.error = False
+        self.screenmanager.get_screen('calculo_de_volume').ids.textbox_numero_de_pessoas.error = False
+        self.screenmanager.get_screen('calculo_de_volume').ids.textbox_temperatura_media.error = False
+        self.screenmanager.get_screen('calculo_de_volume').ids.textbox_intervalo_limpeza.error = False
+
+        # Filtro Anaeróbio
+        self.screenmanager.get_screen('filtro_anaerobio').ids.dropdown_tipo_de_edificacao.text = "Selecione uma opção"
+        self.screenmanager.get_screen('filtro_anaerobio').ids.textbox_numero_de_pessoas.text = ""
+
+        self.screenmanager.get_screen('filtro_anaerobio').ids.dropdown_tipo_de_edificacao.error = False
+        self.screenmanager.get_screen('filtro_anaerobio').ids.textbox_numero_de_pessoas.error = False
+
+        # Sumidouro/Vala de Infiltração
+        self.screenmanager.get_screen('sumidouro').ids.dropdown_tipo_de_edificacao.text = "Selecione uma opção"
+        self.screenmanager.get_screen('sumidouro').ids.textbox_numero_de_pessoas.text = ""
+        self.screenmanager.get_screen('sumidouro').ids.textbox_coeficiente_percolacao.text = ""
+        
+        self.screenmanager.get_screen('sumidouro').ids.dropdown_tipo_de_edificacao.error = False
+        self.screenmanager.get_screen('sumidouro').ids.textbox_numero_de_pessoas.error = False
+        self.screenmanager.get_screen('sumidouro').ids.textbox_coeficiente_percolacao.error = False
     
     def change_tipo_de_edificacao_screen(self, screen):
         global CURRENT_SCREEN
@@ -141,12 +178,17 @@ class Fossilita(MDApp):
         self.screenmanager.current = "home"
         self.screenmanager.transition.direction = "right"
 
+        self.clear_all()
+
         global APP
         APP = self
 
     def return_right_to(self, target):
         self.screenmanager.current = target
         self.screenmanager.transition.direction = "right"
+
+        if (target == "home"):
+            self.clear_all()
 
         global APP
         APP = self
@@ -168,10 +210,29 @@ class Fossilita(MDApp):
         APP = self
 
     def calculate_volume_util(self):
-        Edificacao = self.screenmanager.get_screen('calculo_de_volume').ids.dropdown_tipo_de_edificacao.text # Tipo de Edificação
-        Np = int(self.screenmanager.get_screen('calculo_de_volume').ids.textbox_numero_de_pessoas.text) # Número de Pessoas
-        Temperatura_media = int(self.screenmanager.get_screen('calculo_de_volume').ids.textbox_temperatura_media.text)  # Temperatura Média
-        Intervalo_limpeza = int(self.screenmanager.get_screen('calculo_de_volume').ids.textbox_intervalo_limpeza.text) # Intervalo de Limpeza
+        if (self.screenmanager.get_screen('calculo_de_volume').ids.dropdown_tipo_de_edificacao.text != "Selecione uma opção"):
+            Edificacao = self.screenmanager.get_screen('calculo_de_volume').ids.dropdown_tipo_de_edificacao.text # Tipo de Edificação
+        else:
+            self.screenmanager.get_screen('calculo_de_volume').ids.dropdown_tipo_de_edificacao.error = True
+            return
+
+        if (self.screenmanager.get_screen('calculo_de_volume').ids.textbox_numero_de_pessoas.text != ""):
+            Np = int(self.screenmanager.get_screen('calculo_de_volume').ids.textbox_numero_de_pessoas.text) # Número de Pessoas
+        else:
+            self.screenmanager.get_screen('calculo_de_volume').ids.textbox_numero_de_pessoas.error = True
+            return
+
+        if (self.screenmanager.get_screen('calculo_de_volume').ids.textbox_temperatura_media.text != ""):
+            Temperatura_media = int(self.screenmanager.get_screen('calculo_de_volume').ids.textbox_temperatura_media.text)  # Temperatura Média
+        else:
+            self.screenmanager.get_screen('calculo_de_volume').ids.textbox_temperatura_media.error = True
+            return
+
+        if (self.screenmanager.get_screen('calculo_de_volume').ids.textbox_intervalo_limpeza.text != ""):
+            Intervalo_limpeza = int(self.screenmanager.get_screen('calculo_de_volume').ids.textbox_intervalo_limpeza.text) # Intervalo de Limpeza
+        else:
+            self.screenmanager.get_screen('calculo_de_volume').ids.textbox_intervalo_limpeza.error = True
+            return
 
         C = False #Contribuição de Esgoto por Unidade
         T = False #Tempo de detenção
@@ -213,19 +274,19 @@ class Fossilita(MDApp):
         Cd = Np * C  #Cálculo da Contribuição Diária
 
         if (Cd <= 1500):
-            T = 24
+            T = 1
         elif (Cd >= 1501 and Cd <= 3000):
-            T = 22
+            T = 0.92
         elif (Cd >= 3001 and Cd <= 4500):
-            T = 20
+            T = 0.83
         elif (Cd >= 4501 and Cd <= 6000):
-            T = 18
+            T = 0.75
         elif (Cd >= 6001 and Cd <= 7500):
-            T = 16
+            T = 0.67
         elif (Cd >= 7501 and Cd <= 9000):
-            T = 14
+            T = 0.58
         elif (Cd >= 9001):
-            T = 12
+            T = 0.5
 
         if (Temperatura_media < 10 and Intervalo_limpeza == 1):
             K = 94
@@ -273,7 +334,7 @@ class Fossilita(MDApp):
         elif (Temperatura_media >= 21 and Intervalo_limpeza == 5):
             K = 217
 
-        Volume_util = round(1000 + Np * (C * T + K + Lf), 2)
+        Volume_util = round(1000 + Np * (C * T + K * Lf), 2)
 
         self.screenmanager.current = "resultado_fossa"
         self.screenmanager.transition.direction = "left"
@@ -284,8 +345,18 @@ class Fossilita(MDApp):
         APP = self
 
     def calculate_filtro_anaerobio(self):
-        Edificacao = self.screenmanager.get_screen('filtro_anaerobio').ids.dropdown_tipo_de_edificacao.text # Tipo de Edificação
-        Np = int(self.screenmanager.get_screen('filtro_anaerobio').ids.textbox_numero_de_pessoas.text) # Número de Pessoas
+
+        if (self.screenmanager.get_screen('filtro_anaerobio').ids.dropdown_tipo_de_edificacao.text != "Selecione uma opção"):
+            Edificacao = self.screenmanager.get_screen('filtro_anaerobio').ids.dropdown_tipo_de_edificacao.text # Tipo de Edificação
+        else:
+             self.screenmanager.get_screen('filtro_anaerobio').ids.dropdown_tipo_de_edificacao.error = True
+             return
+
+        if (self.screenmanager.get_screen('filtro_anaerobio').ids.textbox_numero_de_pessoas.text != ""):
+            Np = int(self.screenmanager.get_screen('filtro_anaerobio').ids.textbox_numero_de_pessoas.text) # Número de Pessoas
+        else:
+            self.screenmanager.get_screen('filtro_anaerobio').ids.textbox_numero_de_pessoas.error = True
+            return
 
         C = False
 
@@ -322,25 +393,25 @@ class Fossilita(MDApp):
         Cd = C * Np
 
         if (Cd <= 1500):
-            T = 24
+            T = 1
 
         elif (Cd >= 1501 and Cd <= 3000):
-            T = 22
+            T = 0.92
 
         elif (Cd >= 3001 and Cd <= 4500):
-            T = 20
+            T = 0.83
 
         elif (Cd >= 4501 and Cd <= 6000):
-            T = 18
+            T = 0.75
 
         elif (Cd >= 6001 and Cd <= 7500):
-            T = 16
+            T = 0.67
 
         elif (Cd >= 7501 and Cd <= 9000):
-            T = 14
+            T = 0.58
 
         elif (Cd >= 9001):
-            T = 12
+            T = 0.5
 
         Volume_util = round(1.60  * Np * C * T, 2)
 
@@ -357,5 +428,67 @@ class Fossilita(MDApp):
         global APP
         APP = self
 
+    def calculate_sumidouro(self):
+        if (self.screenmanager.get_screen('sumidouro').ids.dropdown_tipo_de_edificacao.text != "Selecione uma opção"):
+            Edificacao = self.screenmanager.get_screen('sumidouro').ids.dropdown_tipo_de_edificacao.text # Tipo de Edificação
+        else:
+            self.screenmanager.get_screen('sumidouro').ids.dropdown_tipo_de_edificacao.error = True
+            return
+
+        if (self.screenmanager.get_screen('sumidouro').ids.textbox_numero_de_pessoas.text != ""):
+            Np = int(self.screenmanager.get_screen('sumidouro').ids.textbox_numero_de_pessoas.text) # Número de Pessoas
+        else:
+            self.screenmanager.get_screen('sumidouro').ids.textbox_numero_de_pessoas.error = True
+            return
+
+        if (self.screenmanager.get_screen('sumidouro').ids.textbox_coeficiente_percolacao.text != ""):
+            Ci = float(self.screenmanager.get_screen('sumidouro').ids.textbox_coeficiente_percolacao.text) # Coeficiente de Percolação
+        else:
+            self.screenmanager.get_screen('sumidouro').ids.textbox_coeficiente_percolacao.error = True
+            return
+
+        C = False
+
+        if(Edificacao == "Residência"):
+            C = 200
+
+        elif(Edificacao == "Alojamento Provisório"):
+            C = 80
+
+        elif(Edificacao == "Fábrica em Geral"):
+            C = 70
+
+        elif(Edificacao == "Escritório"):
+            C = 50
+
+        elif(Edificacao == "Edifícios Públicos ou Comerciais"):
+            C = 50
+
+        elif(Edificacao == "Escolas ou Locais de Longa Permanência"):
+            C = 50
+
+        elif(Edificacao == "Bares"):
+            C = 6
+
+        elif(Edificacao == "Restaurantes e Similares"):
+            C = 25
+
+        elif(Edificacao == "Cinemas, Teatros e Locais de Curta Permanência"):
+            C = 2
+
+        elif(Edificacao == "Sanitários Públicos"):
+            C = 480
+
+        V = C*Np
+
+        A = round(V/Ci, 2)
+
+        self.screenmanager.current = "resultado_sumidouro"
+        self.screenmanager.transition.direction = "left"
+
+        self.screenmanager.get_screen('resultado_sumidouro').ids.label_resultado_area.text = f'{A} m²'
+
+        global APP
+        APP = self
 
 Fossilita().run()
